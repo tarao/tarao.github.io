ba212920259b26abb4a3b62e78aee696cc175199
tarao <tarao.gnn@gmail.com>
2026-04-08 17:57:49 +0900

    Debug.

--- _plugins/tags/oembed.rb
+++ _plugins/tags/oembed.rb
@@ -45,6 +45,7 @@ module Jekyll
       url = Liquid::Template.parse(@text).render context
       url.strip!
 
+      $stderr.puts("::OEmbed::providers.get(#{url})")
       result = cache.getset(url) do
         begin
           # oembed look up
